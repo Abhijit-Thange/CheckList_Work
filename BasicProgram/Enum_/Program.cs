741728943d21b638ae9bc14b04d5a02f6fcffe5c
Abhijit-Thange <thangeabhijit1998@gmail.com>
2023-04-04 19:19:44 +0530

    Enum

---
+++ BasicProgram/Enum_/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test r = Test.third;
+            Console.WriteLine((int)Test.eight);
+            Console.ReadLine();
+        }
+    }
+
+    enum Test
+    {
+        first=1, 
+        second=2,
+        third=3,
+        seven=7,
+        eight
+    }
+
+}
