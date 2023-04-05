0301f35aa34a5bf9c22f28a798225412a8eb37aa
Abhijit-Thange <thangeabhijit1998@gmail.com>
2023-04-05 17:14:16 +0530

    IEnumerable_VS_IEnumerator

--- BasicProgram/IEnumarable_&_IEnumarator/Program.cs
+++ BasicProgram/IEnumarable_&_IEnumarator/Program.cs
@@ -19,19 +19,66 @@ namespace IEnumarable___IEnumarator
             list.Add(6);
             list.Add(7);
 
-            IEnumerable<int> enumerable = list.ToArray();
+            IEnumerable<int> enumerable = list; //IEnumerable don't know his current state of cursor
+            Range0To5(enumerable);
             foreach (int i in enumerable)
             {
-                Console.WriteLine(i);
+              //  Console.WriteLine(i);
             }
 
-            IEnumerator<int> enumerator = enumerable.GetEnumerator();
+            IEnumerator<int> enumerator = enumerable.GetEnumerator(); //Ienumartor know his current state of cursor
+           // RatorRange0To5(enumerator);
             while (enumerator.MoveNext())
-            {
-                Console.WriteLine(enumerator.Current);
+            { 
+               // Console.WriteLine(enumerator.Current);
             }
 
             Console.ReadLine();
         }
+
+        public static void Range0To5(IEnumerable<int> enumerable)
+        {
+            Console.WriteLine("IEnumerable Output Less Than 5.....");
+            foreach (int i in enumerable)
+            {
+                Console.WriteLine(i);
+                if(i>5)
+                {
+                    Range5toAbove(enumerable);
+                }
+            }              
+        }
+
+        public static void Range5toAbove(IEnumerable<int> enumerable)
+        {
+            Console.WriteLine("IEnumerable Output Above 5.....");  
+            foreach (int i in enumerable)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        static void RatorRange0To5(IEnumerator<int> enumerable)
+        {
+            Console.WriteLine("IEnumerator Output Less Than 5.....");
+            while (enumerable.MoveNext())
+            {
+                Console.WriteLine(enumerable.Current);
+                if (enumerable.Current > 5)
+                {
+                    RatorRange5ToAbove(enumerable);
+                }
+            }
+        }
+
+        static void RatorRange5ToAbove(IEnumerator<int> enumerable)
+        {
+            Console.WriteLine("IEnumerator Output More Than 5.....");
+            while (enumerable.MoveNext())
+            {
+                Console.WriteLine(enumerable.Current);
+            }
+        }
+
     }
 }
