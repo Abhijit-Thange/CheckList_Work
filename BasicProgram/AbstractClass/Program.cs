c49752677e8a680553b70c04de0b251204d79ea6
Abhijit-Thange <thangeabhijit1998@gmail.com>
2023-04-07 19:47:22 +0530

    Abstraction, Abstract Class, Interface

--- BasicProgram/AbstractClass/Program.cs
+++ BasicProgram/AbstractClass/Program.cs
@@ -6,24 +6,67 @@ using System.Threading.Tasks;
 
 namespace AbstractClass
 {
-    internal class Program
+    class Simple
     {
-        static void Main(string[] args)
-        {
-        }
-    }
 
-   abstract class A
+    }
+    abstract class Super
     {
+        public abstract void super();
+        
+    }
+   abstract class Person:Super
+    {
+        public abstract uint Id { get; set; }
+        public abstract string Name { get;set; }
         public abstract void Run();
         public abstract void Test();
     }
 
-    class B : A
+    class Student : Person
     {
+        public uint StudentId;
+        public string StudentName;
+        public override uint Id 
+        { get
+            {
+                return this.StudentId;
+            }
+            set { this.StudentId = value; }
+                
+         }
+        public override string Name
+        {
+            get
+            {
+                return this.StudentName;
+            }
+            set
+            {
+                this.StudentName = value;   
+            }
+        }
+
         public override void Run() { }
 
         public override void Test() { }
+
+        public override void super()
+        {
+            throw new NotImplementedException();
+        }
     }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.Id = 1;
+            student.Name = "Test";
+            Console.WriteLine(student.Name);
+            Console.ReadLine();    
+        }
+    }
+
 
 }
